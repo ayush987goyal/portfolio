fa0629fd269a2517a2bc2f38ab612731d0471d3b
Ayush Goyal <ayush987goyal@gmail.com>
2018-03-02 02:05:04 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,67 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+
+    <v-navigation-drawer app temporary v-model="drawer">
+      <v-toolbar flat>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title class="title">
+              {{ title }}
+            </v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+      <v-divider></v-divider>
+      <v-list dense class="pt-0">
+        <v-list-tile ripple v-for="item in items" :key="item.name" @click="drawer = !drawer">
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.name }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar fixed app>
+      <v-toolbar-side-icon class="hidden-md-and-up"  @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat v-for="item of items" :key="item.name">{{ item.name }}</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <v-content>
+      <v-container>
+        <intro /> 
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2018</span>
+    </v-footer>
+
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import intro from './components/intro';
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  components: {
+    intro
+  },
+  data() {
+    return {
+      title: 'Ayush Goyal',
+      drawer: false,
+      items: [
+        { name: 'Experience', target: '#experience' },
+        { name: 'Education', target: '#education' },
+        { name: 'Portfolio', target: '#portfolio' },
+        { name: 'Skills', target: '#skills' },
+        { name: 'Contact', target: '#contact' }
+      ]
+    };
+  }
+};
+</script>
---
+++ src/components/intro.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="intro">
+
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.intro {
+  height: calc(100vh - 64px);
+
+  background-image: url(../assets/img/business-laptop.jpg);
+  background-size: cover;
+}
+</style>
